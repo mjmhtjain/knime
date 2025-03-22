8aa4466621f4bdfb3b96ebab5420676fb03d6f6e
mjmhtjain <42150844+mjmhtjain@users.noreply.github.com>
2025-03-22 21:51:36 +0100

    running for 10 seconds

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,10 +1,27 @@
 package main
 
-import "github.com/mjmhtjain/knime/src/outbox"
+import (
+	"fmt"
+	"time"
+
+	"github.com/mjmhtjain/knime/src/outbox"
+)
 
 func main() {
 	outboxClient := outbox.New()
 
-	msg := outbox.NewMessage("test", "test")
-	outboxClient.PostMessage(msg)
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	count := 0
+	for range ticker.C {
+		count++
+		msg := outbox.NewMessage(fmt.Sprintf("subject-%d", count), fmt.Sprintf("body-%d", count))
+		outboxClient.PostMessage(msg)
+
+		if count >= 10 {
+			fmt.Println("10 seconds elapsed. Exiting.")
+			return
+		}
+	}
 }
