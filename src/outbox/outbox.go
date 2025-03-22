fbe706ae70b5fb6baaaa17be7428c01af5d4d76b
mjmhtjain <42150844+mjmhtjain@users.noreply.github.com>
2025-03-22 21:19:24 +0100

    separation

--- src/outbox/outbox.go
+++ src/outbox/outbox.go
@@ -1,20 +1,30 @@
 package outbox
 
+import (
+	"github.com/mjmhtjain/knime/src/internal/obj"
+	"github.com/mjmhtjain/knime/src/internal/service"
+)
+
 var outboxIns *Outbox
 
 type Outbox struct {
+	messageService service.IMessageService
 }
 
 func New() *Outbox {
 	if outboxIns == nil {
-		outboxIns = &Outbox{}
+		outboxIns = &Outbox{
+			messageService: service.NewMessageService(),
+		}
 	}
 
 	// TODO: initialize NATS for sending the messages
 	// TODO: initialize DB for persisting the messages
+	// TODO: initialize the internal
 	return outboxIns
 }
 
 func (o *Outbox) PostMessage(message *Message) error {
-	return nil
+	msg := obj.NewMessage(message.Subject, message.Body)
+	return o.messageService.SaveMessage(msg)
 }
